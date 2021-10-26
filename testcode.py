61d7159e1719fc80f104ce2a6154a748a63165c7
MuaazC <choudharymuaaz@gmail.com>
2021-10-26 13:23:19 +0100

    Create testcode.py
    this code shows multiples of 7 up to 100 then continues on with multiples of 8 up to 200 and finishes on multiples of 9 up to 300

---
+++ testcode.py
@@ -0,0 +1,14 @@
+i=0
+for x in range(0,300):
+  if x< 100:
+    if x%7==0:
+      i=x
+      print(i)
+  if x<200 and x > 100:
+    if x%8==0:
+      i=x
+      print(i)
+  if x<300 and x > 200:
+    if x%9==0:
+      i=x
+      print(i) 
